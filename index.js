d9b9ccbb7c6104661894add91bfd6f018f418cb9
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-10-21 08:45:47 +0600

    added news-caterogy with id

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ const port = process.env.POR || 5000;
 const cors = require('cors')
 app.use(cors());
 const categories = require('./data/Category/category.json');
+const news = require('./news.json');
 
 app.get('/', (req, res) => {
     res.send("News API is running Now!");
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    } else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.get('/news-category', (req, res) => {
